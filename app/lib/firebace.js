5aee45e4607fd61631f89ffde4675f2ed772e9d5
giorgi kapanadze <georgegiorgika@gmail.com>
2025-02-16 02:37:18 +0000

    inplement add and read info

--- app/lib/firebace.js
+++ app/lib/firebace.js
@@ -7,15 +7,15 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC_H__QNb-U_qESJjYYqLwsoAJieVpqJzU",
-  authDomain: "mallaghan-d35c2.firebaseapp.com",
-  projectId: "mallaghan-d35c2",
-  storageBucket: "mallaghan-d35c2.firebasestorage.app",
-  messagingSenderId: "806639679397",
-  appId: "1:806639679397:web:b4f7c946618994d8775a4c",
-  measurementId: "G-BHHVFJ6H3Q",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_H__QNb-U_qESJjYYqLwsoAJieVpqJzU",
+//   authDomain: "mallaghan-d35c2.firebaseapp.com",
+//   projectId: "mallaghan-d35c2",
+//   storageBucket: "mallaghan-d35c2.firebasestorage.app",
+//   messagingSenderId: "806639679397",
+//   appId: "1:806639679397:web:b4f7c946618994d8775a4c",
+//   measurementId: "G-BHHVFJ6H3Q",
+// };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
